7a3ea5a3081eb1cad8c6f7297803cc5c559df2cb
Esther Ojinji <ojinjiester@gmail.com>
2024-05-01 14:20:45 +0200

    Replacing useEffect with React Query

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,31 @@
 import React from 'react';
 import {createRoot} from 'react-dom/client';
 import {BrowserRouter, Routes, Route, Link} from 'react-router-dom';
+import {QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import SearchParams from './SearchParams';
 import Details from './Details';
 
-
+const queryClient = new QueryClient({
+    defaultOptions:{
+        queries:{
+            staleTime: Infinity,
+            cacheTime: Infinity,
+        },
+    },
+});
 
 const App = () =>{
    return(
      <BrowserRouter>
-     <header>
-        <Link to='/'>Adopt Me!</Link>
-     </header>
-      <h1>Adopt Me!</h1>
-      <Routes>
-        <Route path='/details/:id' element = {<Details/>}></Route>
-        <Route path='/' element = {<SearchParams/>}></Route>
-      </Routes>
-       
+         <QueryClientProvider client={queryClient}>
+              <header>
+                 <Link to='/'>Adopt Me!</Link>
+              </header>
+              <Routes>
+                 <Route path='/details/:id' element = {<Details/>}></Route>
+                 <Route path='/' element = {<SearchParams/>}></Route>
+              </Routes>
+         </QueryClientProvider> 
      </BrowserRouter>
    );
 }; 
