c6f0a3d8d2fa8777e40020d3cfeeb7c2215cef1d
Esther Ojinji <ojinjiester@gmail.com>
2024-05-01 09:37:57 +0200

    React Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,24 @@
 import React from 'react';
 import {createRoot} from 'react-dom/client';
+import {BrowserRouter, Routes, Route, Link} from 'react-router-dom';
 import SearchParams from './SearchParams';
+import Details from './Details';
+
 
 
 const App = () =>{
    return(
-    <div>
-        <h1>Adopt Me!</h1>
-        <SearchParams/>
-    </div>
+     <BrowserRouter>
+     <header>
+        <Link to='/'>Adopt Me!</Link>
+     </header>
+      <h1>Adopt Me!</h1>
+      <Routes>
+        <Route path='/details/:id' element = {<Details/>}></Route>
+        <Route path='/' element = {<SearchParams/>}></Route>
+      </Routes>
+       
+     </BrowserRouter>
    );
 }; 
 
